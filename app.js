012ce4bb54e220f52ae76426d27d93de11ee55fc
milanpaudell <milanpaudel31@gmail.com>
2024-02-13 00:30:40 +0545

    Project Backend Start, CR Operation

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const app = express()
 
 
 const connectToDatabase = require('./database')
+const Book = require('./model/bookModel')
 
 //Alternative 
 // Const app = require('express')()
 
 
 
+app.use(express.json())
+// app.use(express.urlencoded({ extended : true }))
 
 
 connectToDatabase()
@@ -23,6 +26,95 @@ app.get("/", (req,res)=>{
 })
 
 
+app.post("/book", async(req,res)=>{
+    const { bookName, bookPrice, isbnNumber, authorName, publishedAt, publication } = req.body
+    // console.log(bookName, bookPrice, isbnNumber, authorName, publishedAt)
+
+   await Book.create({
+        // bookName : bookName,
+        // bookPrice: bookPrice,
+        // isbnNumber : isbnNumber,
+        // authorName : authorName,
+        // publishedAt : publishedAt,
+        // publication : publication
+        bookName,
+        bookPrice,
+        isbnNumber,
+        authorName,
+        publishedAt,
+        publication
+    })
+
+
+
+    res.status(201).json({
+        message: "Book created Successfully"
+    })
+})
+
+
+// all read
+app.get("/book", async(req,res)=>{
+    const books = await Book.find()     //return array ma garxa
+    console.log(books)
+    res.status(200).json({
+        message : "Book fetched Successfully",
+        data : books
+    })
+})
+
+
+// single read
+app.get("/book/:id", async(req,res)=>{
+    const id = req.params.id
+    const books = await Book.findById(id)   //return onject garxa
+    // console.log(books)
+
+    if(!books){
+        return res.status(404).json({
+            message : "Book not found"
+        })
+
+    }else{
+        res.status(200).json({
+            message : "Book fetched Successfully",
+            data : books
+        })
+
+    }
+})
+
+
+
+// single read
+// app.get("/book/:id", async(req,res)=>{
+//     try{
+//         const id = req.params.id
+//     const books = await Book.findById(id)   //return onject garxa
+//     // console.log(books)
+
+//     if(!books){
+//         return res.status(404).json({
+//             message : "Book not found"
+//         })
+
+//     }else{
+//         res.status(200).json({
+//             message : "Book fetched Successfully",
+//             data : books
+//         })
+
+//     }
+
+//     }
+
+//     catch(error){
+//         res.status(500).json({
+//             message : error.message
+//         })
+//     }
+    
+// })
 
 
 app.listen(3000,() => {
---
+++ model/bookModel.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+// const bookSchema = new mongoose.Schema({
+
+const Schema = mongoose.Schema
+const bookSchema = new Schema({
+
+    bookName :{
+        type : String,
+        unique : true
+    },
+    bookPrice :{
+        type : String
+    },
+    isbnNumber :{
+        type : Number
+    },
+    authorName :{
+        type : String
+    },
+    publishedAt :{
+        type : String
+    },
+    publication :{
+        type : String
+    }
+
+})
+
+// class Schema{
+//     Schema(title){
+//         console.log(title)
+//     }
+// }
+
+// const schema = new Schema("Hello")
+
+
+const Book = mongoose.model('Book', bookSchema)
+module.exports = Book
